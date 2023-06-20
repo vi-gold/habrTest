33e191cc98de9290b6fa3fd7bb7cb30dba4592c3
vi-gold <popovpavelwork@gmail.com>
2023-06-20 21:07:32 +0500

    fix tests

--- src/test/java/com/example/habrtest/MainPageTest.java
+++ src/test/java/com/example/habrtest/MainPageTest.java
@@ -9,6 +9,8 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 import java.time.Duration;
 
@@ -34,25 +36,29 @@ public class MainPageTest {
     }
 
     @Test
+    @RepeatedTest(3)
     public void checkHeadText() {
 
         WebElement administrationLink = driver.findElement(By.xpath("//a[contains(text(), 'Администрирование')]"));
+        String admLinkText = administrationLink.getText();
         administrationLink.click();
 
+        WebDriverWait wait = new WebDriverWait(driver,Duration.ofSeconds(10));
+        wait.until(ExpectedConditions.invisibilityOfElementWithText(By.cssSelector("h1.tm-section-name__text"), "Все потоки"));
+
         WebElement header = driver.findElement(By.cssSelector("h1.tm-section-name__text"));
-        System.out.println(administrationLink.getText());
-        System.out.println(header.getText());
-        assertEquals(administrationLink.getText(), header.getText(), "Названия ссылки и заголовка не совпадают");
+        assertEquals(admLinkText, header.getText(), "Названия ссылки и заголовка не совпадают");
     }
 
     @Test
-    public void checkComments() {
+    @RepeatedTest(3)
+    public void checkTextInSearch() {
 
-        WebElement news = driver.findElement(By.cssSelector("a[data-test-id = 'tab-link-active']"));
-        news.click();
+        WebElement searchIcon = driver.findElement(By.cssSelector("svg.tm-header-user-menu__icon_search"));
+        searchIcon.click();
 
-        assertTrue(driver.findElement(By.xpath("//h2[contains(text(), 'Читают сейчас')]")).isDisplayed(),
-                "Блок \"Читают сейчас\" отсутствует");
+        assertTrue(driver.findElement(By.cssSelector("h2.tm-block__title")).isDisplayed(),
+                "Заголовок \"Минуточку внимания\" отсутствует");
     }
 
 }
